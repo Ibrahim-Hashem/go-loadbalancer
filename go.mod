0e376bb8b7e9081e92a7554f69fab082ffc9cbe8
Ibrahim-Hashem <ibrahimsgh6@hotmail.co.uk>
2024-05-05 15:44:59 +0100

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ibrahim-Hashem/go-loadbalancer
+
+go 1.22.2
---
+++ src/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http/httputil"
+)
+
+type simpleServer struct {
+	addr string
+	proxy *httputil.ReverseProxy
+	
+}
